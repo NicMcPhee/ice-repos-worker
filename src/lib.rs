0f5b83b419c182883b0eb6c007810c0c19789e97
Nic McPhee <mcphee@morris.umn.edu>
2022-11-01 13:57:05 -0500

    Some early flailings
    This is the result of the first streaming session working on the Cloudflare Workers and GitHub OAuth. We manage to get the initial stage of authorization to work, but I wasn't able to get a token from GitHub after that.

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,45 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
     router
         .get("/", |_, _| Response::ok("Hello from Workers!"))
+        .get_async("/finalize_login", |req, ctx| async move {
+            console_log!("We're in finalize login");
+            let url = web_sys::Url::new(&req.url().unwrap().as_str()).unwrap();
+            let query_params = url.search_params();
+            let code = query_params.get("code").unwrap();
+            let state = query_params.get("state").unwrap();
+            console_log!("The code is {code} and the state is {state}.");
+
+            {
+                let code = code.clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    console_log!("We're in the spawn_local future");
+                    let request_url = format!("https://github.com/login/oauth/access_token?client_id=c5b735f256dadf835133&client_secret=1f213f76072f25eb76dabb2a28c39fc93b9dd268&code={code}");
+                    let request = gloo_net::http::Request::put(&request_url)
+                        .query([("client_id", "c5b735f256dadf835133"),
+                                // TODO: Get client secret from the environment.
+                                ("client_secret", "xxxx"),
+                                ("code", &code)])
+                        .header("Accept", "application/json");
+                    console_log!("We've constructed the request {request:?}");
+                    let response = request.send().await;
+                    console_log!("Response was {response:?}.");
+                    let response = match response {
+                        Ok(response) => response,
+                        Err(err) => { console_log!("{err}"); panic!("{}", err) }
+                    };
+                    console_log!("We've sent the request");
+                    let response_url = web_sys::Url::new(&response.url()).unwrap();
+                    let response_params = response_url.search_params();
+                    let response_token = response_params.get("access_token").unwrap();
+                    let response_token_type = response_params.get("token_type").unwrap();
+                    let response_scope = response_params.get("scope").unwrap();
+
+                    console_log!("We have a token with {response_token}, {response_token_type}, and {response_scope}.");
+                });
+            }
+
+            Response::ok(format!("We are logged in with {code} and {state}."))
+        })
         .post_async("/form/:field", |mut req, ctx| async move {
             if let Some(name) = ctx.param("field") {
                 let form = req.form_data().await?;
